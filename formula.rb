6b861212dd923b4251652b2d327007ed6671560c
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2017-04-18 16:19:47 -0400

    add brew formula

---
+++ formula.rb
@@ -0,0 +1,11 @@
+class BSPInstall < Formula
+  desc "..."
+  homepage "https://github.com/OttoHagg/brightspot-server-install"
+  url #TODO needs URL of release tar.gz
+  version #TODO needs release version number
+  sha256 #TODO checksum of tarball
+
+  def install
+    bin.install "bsp-install"
+  end
+end
